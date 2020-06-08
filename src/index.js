1f271a2fe8c8b8d3bf9c955ab46c05575bb0974e
Tana1509 <tana1509@gmail.com>
2020-06-08 17:03:16 +0300

    filter fixes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 jQuery(document).ready(function($) {
   //burger
-  let menuBtn = $('.header__burger');
-  menuBtn.on('click', function() {
-    let navigation = $('.header__nav__list');
-    navigation.toggleClass('active');
+  let menuBtn = $(".header__burger");
+  menuBtn.on("click", function() {
+    let navigation = $(".header__nav__list");
+    navigation.toggleClass("active");
   });
 
   // кнопка "наверх"
   $(window).scroll(function() {
     if ($(this).scrollTop() > 0) {
-      $('.to_top').fadeIn();
+      $(".to_top").fadeIn();
     } else {
-      $('.to_top').fadeOut();
+      $(".to_top").fadeOut();
     }
   });
-  $('.to_top').click(function() {
-    $('body,html').animate(
+  $(".to_top").click(function() {
+    $("body,html").animate(
       {
         scrollTop: 0,
       },
@@ -23,6 +23,14 @@ jQuery(document).ready(function($) {
     );
     return false;
   });
+  $(".heading__slaider").slick({
+    nextArrow: document.querySelector("#owl-nav_arrow_prev"),
+    prevArrow: document.querySelector("#owl-nav_arrow_next"),
+  dots: true
+  });
+
+
+
   // $('.heading__slaider').owlCarousel({
   //   loop: true,
   //   margin: 10,
@@ -34,9 +42,9 @@ jQuery(document).ready(function($) {
   //     },
   //   },
   // });
-
+  //
   // // Передача полномочий кастомным кнопкам
-
+  //
   // let owl = $('.owl-carousel');
   // owl.owlCarousel();
   // //$(".next") - находим нашу кнопку
@@ -48,18 +56,18 @@ jQuery(document).ready(function($) {
   // });
 
   //form validation
-  $('.blog__post__form_name').on('input', function() {
-    let count = $('.blog__post__form_name').val().length;
+  $(".blog__post__form_name").on("input", function() {
+    let count = $(".blog__post__form_name").val().length;
     console.log(count);
     let minCount = 3;
   });
-  $('.blog__post__form_message').on('input', function() {
-    let count = $('.blog__post__form_message').val().length;
+  $(".blog__post__form_message").on("input", function() {
+    let count = $(".blog__post__form_message").val().length;
     console.log(count);
     let minCount = 3;
   });
-  $('.blog__post__form_website').on('input', function() {
-    let count = $('.blog__post__form_website').val().length;
+  $(".blog__post__form_website").on("input", function() {
+    let count = $(".blog__post__form_website").val().length;
     console.log(count);
     let minCount = 3;
     // if (count < minCount) {
@@ -68,112 +76,115 @@ jQuery(document).ready(function($) {
   });
   // создаем функцию для обработки события submit
 
-  let btn = $('.form__btn');
-  btn.on(`click`, function() {
-    let nameLength = $('.blog__post__form_name').val().length;
+  let btn = $(".form__btn");
+  let jQuery = btn.on(`click`, function() {
+    let nameLength = $(".blog__post__form_name").val().length;
     if (nameLength > 3) {
-      console.log('Поле валидно');
+      console.log("Поле валидно");
     } else {
-      console.log('Имя слишком короткое');
+      console.log("Имя слишком короткое");
     }
-    let nameWebsite = $('.blog__post__form_website').val().length;
+    let nameWebsite = $(".blog__post__form_website").val().length;
     if (nameWebsite > 3) {
-      console.log('Поле валидно');
+      console.log("Поле валидно");
     } else {
-      console.log('Имя Website слишком короткое');
+      console.log("Имя Website слишком короткое");
     }
-    let nameMessage = $('.blog__post__form_message').val().length.trim;
+    let nameMessage = $(".blog__post__form_message").val().length.trim;
     if (nameMessage > 10) {
-      console.log('Поле валидно');
+      console.log("Поле валидно");
     } else {
-      console.log('Пожалуйста, раскройте шире Ваш вопрос');
+      console.log("Пожалуйста, раскройте шире Ваш вопрос");
     }
     // короткая запись if и else
-    let isName = $('.blog__post__form_name').val.trim;
+    let isName = $(".blog__post__form_name").val.trim();
     {
       isName
-        ? console.log('все хорошо')
-        : console.log('поле не может быть пустым');
+        ? console.log("все хорошо")
+        : console.log("поле не может быть пустым");
     }
   });
 
   // создаем функцию для обработки события submit
-  $('.blog__post__form').on('submit', function(e) {
+  $(".blog__post__form").on("submit", function(e) {
     e.preventDefault();
   });
 
   // переключени темы
-  let body = $('body');
-  let switchBtn = $('.switch-btn');
-  if (localStorage.getItem('theme') === 'dark') {
-    body.addClass('dark');
-    switchBtn.addClass('switch-on');
+  let body = $("body");
+  let switchBtn = $(".switch-btn");
+  if (localStorage.getItem("theme") === "dark") {
+    body.addClass("dark");
+    switchBtn.addClass("switch-on");
   }
-  switchBtn.on('click', function() {
-    $(this).toggleClass('switch-on');
+  switchBtn.on("click", function() {
+    $(this).toggleClass("switch-on");
 
-    if ($(this).hasClass('switch-on')) {
-      localStorage.setItem('theme', 'dark');
+    if ($(this).hasClass("switch-on")) {
+      localStorage.setItem("theme", "dark");
     } else {
-      localStorage.setItem('theme', 'light');
+      localStorage.setItem("theme", "light");
     }
 
-    if (localStorage.getItem('theme') === 'dark') {
-      body.addClass('dark');
+    if (localStorage.getItem("theme") === "dark") {
+      body.addClass("dark");
     }
-    if (localStorage.getItem('theme') === 'light') {
-      body.removeClass('dark');
+    if (localStorage.getItem("theme") === "light") {
+      body.removeClass("dark");
     }
   });
 
   // фильтр
-  let button = $('.filter__nav');
-  button.on('click', function() {
+  let button = $(".filter__nav");
+  button.on("click", function() {
     $(this)
-      .addClass('active')
+      .addClass("active")
       .siblings()
-      .removeClass('active');
-    let cards = $('.home__page__filter__work');
-    cards.removeClass('active');
+      .removeClass("active");
+    let cards = $(".home__page__filter__work");
+    cards.removeClass("active");
     let btnText = $(this)
       .text()
       .toLowerCase();
     let final = cards.filter(function() {
-      if (btnText === 'all') {
+      if (btnText === "all") {
         return cards;
       } else {
-        return $(this).data('category') === btnText;
+        return $(this).data("category") === btnText;
       }
     });
     final.each(function() {
-      $(this).addClass('active');
+      $(this).addClass("active");
     });
   });
-
-  // $(".slider__pic").slick({
-  //   autoplay: false,
-  //   dots: true,
-  //   customPaging: function (slider, i) {
-  //     let thumb = $(slider.$slides[i]).data('thumb');
-  //     return '<a><img src="../img/portfolio_slider1.png' + thumb + '"></a>';
-  //   },
+// переход по ссылке
+  // let shareBtn = $(".home__page__filter__work");
+  // shareBtn.on("click", function () {
+  //   document.location.href = "http://www.site.ru";
   // });
-  $('.slider__pic').slick({
+
+  $(".slider__pic").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.slider-nav'
+    asNavFor: ".slider-nav",
   });
-  $('.slider-nav').slick({
+  $(".slider-nav").slick({
     slidesToShow: 4,
     // slidesToScroll: 0,
-    asNavFor: '.slider__pic',
+    asNavFor: ".slider__pic",
     // dots: true,
     centerMode: true,
     focusOnSelect: true,
     speed: 1000,
-    infinite: false
+    infinite: false,
   });
-  
+
+  // добавляем функцию он клик для кнопки share после слайдера
+  let shareBtn = $(".socials__btn");
+  shareBtn.on("click", function() {
+    let navigation = $(".socials__icons");
+    navigation.toggleClass("active");
+  });
 });
